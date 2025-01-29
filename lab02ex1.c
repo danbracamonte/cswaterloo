a4e7e82cd8f14ee4705469081d092a5d9dd1985e
danbracamonte <dbracamonte@gmail.com>
2025-01-28 21:12:14 -0500

    Update lab02ex1.c

--- lab02ex1.c
+++ lab02ex1.c
@@ -15,8 +15,22 @@ int main(int argc, char** argv) {
     // Copy the file path to the buffer without size check
     strcpy(buffer, argv[1]); 
 
+    // Use the buffer in a subsequent function call 
+    // This increases the likelihood of CodeQL detecting the buffer overflow
+    char command[50];
+    snprintf(command, sizeof(command), "ls -l %s", buffer); 
+
+    // This function call is potentially vulnerable 
+    // due to the use of the potentially corrupted buffer
+    FILE *fp = fopen(buffer, "r"); 
+    if (fp == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    fclose(fp);
+
     struct stat file_stat;
-    if (stat(buffer, &file_stat) != 0 || !S_ISREG(file_stat.st_mode)) {
+    if (stat(argv[1], &file_stat) != 0 || !S_ISREG(file_stat.st_mode)) {
         fprintf(stderr, "Invalid file. Please provide a valid file path.\n");
         return -1;
     }
