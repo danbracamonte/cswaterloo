0bf5805502402d35ade790fc270625a0b26ed43c
danbracamonte <dbracamonte@gmail.com>
2025-01-28 20:15:33 -0500

    Update lab02ex1.c

--- lab02ex1.c
+++ lab02ex1.c
@@ -1,18 +1,19 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
-#define BUFSIZE 256
-    
-// This program prints the size of a specified file in bytes
+#include <sys/stat.h>
+
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file path>\n", argv[0]);
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    encodeShellString(cmd, BUFSIZE); 
-    strcat(cmd, argv[2]);
-    system(cmd);
+
+    struct stat file_stat;
+    if (stat(argv[1], &file_stat) != 0 || !S_ISREG(file_stat.st_mode)) {
+        fprintf(stderr, "Invalid file. Please provide a valid file path.\n");
+        return -1;
+    }
+
+    printf("The size of the file is %ld bytes.\n", file_stat.st_size);
+    return 0;
 }
