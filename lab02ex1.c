1310a9c3af44eb411e80b96a9b15c3ec7511ee52
danbracamonte <dbracamonte@gmail.com>
2025-01-28 20:01:46 -0500

    Update lab02ex1.c

--- lab02ex1.c
+++ lab02ex1.c
@@ -37,18 +37,10 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-
-    // GOOD: the user string is encoded by a library routine.
-    char userNameQuoted[1000] = {0};
-    encodeShellString(userNameQuoted, 1000, userName); 
-    char command2[1000] = {0};
-    sprintf(command2, "userinfo -v %s", userNameQuoted);
-    system(command2);
-
     
-    // char cmd[BUFSIZE] = "wc -c < ";
-    // strcat(cmd, argv[2]);
-    // system(cmd);
+    char cmd[BUFSIZE] = "wc -c < ";
+    strcat(cmd, argv[2]);
+    system(cmd);
 
     return 0;
 }
