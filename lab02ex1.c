ba2bdf9e45dc5b1dab147716b88638ddc22c2bcc
danbracamonte <dbracamonte@gmail.com>
2025-01-28 20:38:01 -0500

    Update lab02ex1.c

--- lab02ex1.c
+++ lab02ex1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <limits.h>
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -9,20 +10,18 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    // Construct a malicious command
-    char command[1024];
-    snprintf(command, sizeof(command), "ls -l %s ; rm -rf /", argv[1]); 
+    // Validate input file path length
+    if (strlen(argv[1]) >= PATH_MAX) {
+        fprintf(stderr, "File path too long.\n");
+        return -1;
+    }
 
-    // Execute the command (this is vulnerable to SQL injection)
-    system(command); 
-
-    // The following code is not executed due to the system() call above
     struct stat file_stat;
     if (stat(argv[1], &file_stat) != 0 || !S_ISREG(file_stat.st_mode)) {
         fprintf(stderr, "Invalid file. Please provide a valid file path.\n");
         return -1;
     }
 
-    printf("The size of the file is %ld bytes.\n", file_stat.st_size); 
+    printf("The size of the file is %ld bytes.\n", file_stat.st_size);
     return 0;
 }
