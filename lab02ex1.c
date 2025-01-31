86ff9b705248ab60c8acb6689fa006e4a282f31f
danbracamonte <dbracamonte@gmail.com>
2025-01-30 22:10:29 -0500

    Update lab02ex1.c

--- lab02ex1.c
+++ lab02ex1.c
@@ -1,28 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
-    }
-
-    // Uncontrolled Data in Path Expression
-    char* filename = argv[1]; 
-
-    // Create a buffer with insufficient size
-    char buffer[10];
-
-    // Copy the filename to the buffer without size check
-    strcpy(buffer, filename); 
-
-    // Use the potentially corrupted buffer in a system call 
-    char command[100];
-    snprintf(command, sizeof(command), "cat %s", buffer); 
-
-    // Execute the command 
-    system(command); 
-
-    return 0;
+// C Program to demonstrate the file pointer 
+#include <stdio.h> 
+  
+int main() 
+{ 
+    // declaring file pointer 
+    FILE* fptr; 
+  
+    // trying to get the size of FILE datatype. 
+    printf("Size of FILE Structure: %d bytes", 
+           sizeof(FILE)); 
+  
+    return 0; 
 }
