3056d05bde77bedb1aee02cb6694806e14e8da3e
danbracamonte <dbracamonte@gmail.com>
2025-02-11 10:01:55 -0500

    Update lab02ex1.c

--- lab02ex1.c
+++ lab02ex1.c
@@ -1,28 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc, char** argv) {
     
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file path>\n", argv[0]);
-        return -1;
-    }
-
-    FILE* fp = fopen(argv[1], "rb");
-    if (fp == NULL) {
-        fprintf(stderr, "Error opening file.\n");
-        return -1;
-    }
-
-    fseek(fp, 0, SEEK_END); // Move file pointer to the end
-    long file_size = ftell(fp); 
-    fclose(fp);
-    
-    if (file_size < 0) {
-        fprintf(stderr, "Error getting file size.\n");
-        return -1;
-    }
-     
-     printf("The size of the file is %d bytes.\n", sizeof(FILE));
-    return 0;
+int main(int argc, char **argv) {
+    char *items[] = {"boat", "car", "truck", "train"};
+    int index = atoi(argv[1]);
+    printf("You selected %s\n", items[index]);
 }
